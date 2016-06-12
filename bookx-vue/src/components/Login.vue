04bc5f8230918afd9525e44096bd044893f05e9b
ae6623 <394263788@qq.com>
2016-06-12 23:44:37 +0800

    login组件框

--- bookx-vue/src/components/Login.vue
+++ bookx-vue/src/components/Login.vue
@@ -1,23 +1,28 @@
 <template>
 	这是登录组件
+	 <div class="popup_login">
+      <x-input title="用户名" placeholder="6~12位" :min="6" :max="12" :value.sync="username"></x-input>
+      <x-input title="密&nbsp;&nbsp;&nbsp;&nbsp;码" placeholder="6~12位" type="password" :min="6" :max="12" class="password" :value.sync="pwd"></x-input>
+    </div>
 	<x-button type="primary" @click="login">登录</x-button>
 </template>
 
 <script>
-	import {XButton} from 'vux'
+	import {XButton,XInput} from 'vux'
 
 	export default {
 	  components: {
-		XButton
+		XButton,XInput
 	  },
 	  data () {
 	    return {
-		  username: '',
+		  	username: '',
 		    pwd:''
 		  }
 	  },
 	  methods: {
 		login (){
+			alert(this.username + " : " + this.pwd)
 			console.log('ahahahah');
 		}
 	  }
